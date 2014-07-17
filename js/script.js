5d8e1227e77e81fcd9328f444bdfc055f01ff343
bouncinglime <mari@shiny.local>
2014-07-17 09:57:07 -0700

    File Organization Changes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//reduce the opacity of the banner if the page is scrolled.
+$(window).scroll(function () {
+  var height = $("body").height();
+  var scrollTop = $("body").scrollTop();
+  var opacity = 1;
+
+  if(scrollTop < 41)
+    {opacity = 1-Math.floor(scrollTop)/100;}
+    else
+    {opacity = 0.6;}
+
+  $("#header").css("opacity", opacity);
+  $("#header").hover(function(){
+    $(this).css("opacity", 1);
+    },function(){
+    $(this).css("opacity", 0.6);
+    });
+});
