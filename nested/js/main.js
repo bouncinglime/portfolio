26f223f2f3da1ae0f70c6df85d172942a451ff8a
bouncinglime <mari@shiny.local>
2014-07-17 13:50:20 -0700

    switching from nested hexagons to aligned hexagons

--- nested/js/main.js
+++ nested/js/main.js
@@ -9,8 +9,8 @@ $(window).scroll(function () {
     else
     {opacity = 0.6;}
 
-  $("#header").css("opacity", opacity);
-  $("#header").hover(function(){
+  $("h1").css("opacity", opacity);
+  $("h1").hover(function(){
     $(this).css("opacity", 1);
     },function(){
     $(this).css("opacity", 0.6);
