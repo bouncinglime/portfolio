eae094f9276671236e5fc9ffea17ad3404a26288
bouncinglime <marielle.hsu@gmail.com>
2015-08-23 21:18:28 -0700

    More php-ifying.
    It's not being served properly, though. The first three worked, but the
    last four have not and I don't know why.

--- index.php
+++ index.php
@@ -2,84 +2,87 @@
 
 <html>
 
-  <head>
-	<!-- everything in header.php -->
-	<?php include('header.php'); ?>
-    <!-- page title -->
-    <title>Marielle Hsu - Portfolio</title>
-  </head>
+	<head>
+		<!-- everything in header.php -->
+		<?php include('header.php'); ?>
+		<!-- page title -->
+		<title>Marielle Hsu - Portfolio</title>
+	</head>
 
-  <!-- MAIN BODY -->
+	<!-- MAIN BODY -->
 
-  <body>
+	<body>
 
-    <!-- actual content -->
-	
-    <div class="narrow">
+		<!-- actual content -->
 
-    <!-- logo (in logo.php) and page name -->
-	<?php include('logo.php'); ?>
-    <h1>Marielle Hsu</h1>
+		<div class="narrow">
 
-    <!-- Summary -->
-      <div class="subtitle">
-		<label>Creating efficient, intuitive designs.</label>
-		<br />Melding the creative and the analytical to get the full picture of an experience.
-		<br />Building empathy at each step of the process.
-		<!--	A fusion of analysis and creativity to inspire experiences.
+			<!-- logo (in logo.php) and page name -->
+			<?php include('logo.php'); ?>
+			<h1>Marielle Hsu</h1>
+
+			<!-- Summary -->
+			<div class="subtitle">
+				<label>Creating efficient, intuitive designs.</label>
+				<br />Melding the creative and the analytical to get the full picture of an experience.
+				<br />Building empathy at each step of the process.
+				<!--	A fusion of analysis and creativity to inspire experiences.
 		Analyzing creativity, and creative approaches to analysis.
 		Addressing root causes of issues with... -->
-      </div>
-	  
-    </div> <!-- Closing "narrow" -->
+			</div>
 
-    <br /> <!-- spacing -->
-    <br /> <!-- spacing -->
+		</div>
+		<!-- Closing "narrow" -->
 
-    <!-- laying out the hexagons -->
-    <div class="honeycombs">
-      <!-- Digital Projects -->
-      <a class="comb" href="digiproj.html">
-	<img src="images/digiproj.png" />
-	<div class="hexlabel">
-	  <p>Digital<br />Projects</p>
-	</div>
-	<span>Digital<br/>Projects</span>
-      </a>
-      <!-- About -->
-      <a class="comb" href="about.html">
-	<img src="images/about.png" />
-	<div class="hexlabel">
-	  <p>About</p>
-	</div>
-	<span>About</span>
-      </a>
-      <!-- Connect -->
-      <a class="comb" href="connect.html">
-	<img src="images/connect/connect.png" />
-	<div class="hexlabel">
-	  <p>Connect</p>
-	</div>
-	<span>Connect</span>
-      </a>
-      <!-- Tangible Projects -->
-      <a class="comb" href="tangproj.html">
-	<img src="images/tangproj.png" />
-	<div class="hexlabel">
-	  <p>Tangible Projects</p>
-	</div>
-	<span>Tangible Projects</span>
-      </a>
-      <!-- Resume -->
-      <a class="comb" href="resume.html">
-	<img src="images/resume.png" />
-	<div class="hexlabel">
-	  <p>Resume</p>
-	</div>
-	<span>Resume</span>
-      </a>
-    </div>
+		<br />
+		<!-- spacing -->
+		<br />
+		<!-- spacing -->
 
-  </body>
+		<!-- laying out the hexagons -->
+		<div class="honeycombs">
+			<!-- Digital Projects -->
+			<a class="comb" href="digiproj.html">
+				<img src="images/digiproj.png" />
+				<div class="hexlabel">
+					<p>Digital<br />Projects</p>
+				</div>
+				<span>Digital<br/>Projects</span>
+			</a>
+			<!-- About -->
+			<a class="comb" href="about.html">
+				<img src="images/about.png" />
+				<div class="hexlabel">
+					<p>About</p>
+				</div>
+				<span>About</span>
+			</a>
+			<!-- Connect -->
+			<a class="comb" href="connect.html">
+				<img src="images/connect/connect.png" />
+				<div class="hexlabel">
+					<p>Connect</p>
+				</div>
+				<span>Connect</span>
+			</a>
+			<!-- Tangible Projects -->
+			<a class="comb" href="tangproj.html">
+				<img src="images/tangproj.png" />
+				<div class="hexlabel">
+					<p>Tangible Projects</p>
+				</div>
+				<span>Tangible Projects</span>
+			</a>
+			<!-- Resume -->
+			<a class="comb" href="resume.html">
+				<img src="images/resume.png" />
+				<div class="hexlabel">
+					<p>Resume</p>
+				</div>
+				<span>Resume</span>
+			</a>
+		</div>
+
+	</body>
 
 </html>
