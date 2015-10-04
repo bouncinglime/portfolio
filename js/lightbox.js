af048921e8be971e1a9e089bd89d54124a2774f6
bouncinglime <marielle.hsu@gmail.com>
2015-10-03 20:32:39 -0700

    New hex images for lightbox.

--- js/lightbox.js
+++ js/lightbox.js
@@ -14,7 +14,7 @@
   var LightboxOptions = (function() {
     function LightboxOptions() {
       this.fadeDuration                = 500;
-      this.fitImagesInViewport         = true;
+      this.fitImagesInViewport         = false;
       this.resizeDuration              = 700;
       this.positionFromTop             = 50;
       this.showImageNumberLabel        = true;
@@ -60,7 +60,7 @@
 	// rendered, so changing order changes display.
     Lightbox.prototype.build = function() {
       var self = this;
-      $("<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div><span class='lb-caption'></span></div><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div></div>").appendTo($('body'));
+      $("<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><span class='lb-caption'></span><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div></div>").appendTo($('body'));
       
       // Cache jQuery objects
       this.$lightbox       = $('#lightbox');
